8faf4a8da2ce3e2401bf9d943199ef004d7dddcc
Phlilip Adelabu <adebisiphilip@gmail.com>
2026-09-21 14:59:41 +0100

    initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    public string? Title { get; set; }
+
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [StringLength(100)]
+    public string? Genre { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    public decimal Price { get; set; }
+}
