9169912e794e245bce8e8e33d1a4703107567344
Phlilip Adelabu <adebisiphilip@gmail.com>
2026-09-22 10:25:22 +0100

    Added some data validations

--- Migrations/20260922090250_AdjustedMovieTable.Designer.cs
+++ Migrations/20260922090250_AdjustedMovieTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Data;
 
@@ -10,9 +11,11 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20260922090250_AdjustedMovieTable")]
+    partial class AdjustedMovieTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,14 +9,22 @@ public class Movie
     public int Id { get; set; }
 
     [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
 
-    [Display(Name = "Release Date"), DataType(DataType.Date)]
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
 
-    [StringLength(100)]
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
     public string? Genre { get; set; }
 
-    [Column(TypeName = "decimal(18, 2)"), Range(1, 300)]
-    public decimal Price { get; set; }
+
 }
